ade9bdfbb82fb8567fc6df12f003181a6e1f9d80
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-10-11 06:59:51 +0530

    repo created

---
+++ wiseStepApp/src/main/java/com/wiseStep/WiseStepApplication.java
@@ -0,0 +1,13 @@
+package com.wiseStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseStepApplication.class, args);
+	}
+
+}
