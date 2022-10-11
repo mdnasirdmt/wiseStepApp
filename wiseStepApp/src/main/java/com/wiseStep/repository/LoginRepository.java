9fbf8aa5c0c1a7a72ba8cabab864671ece758d3a
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-10-11 13:13:07 +0530

    template created

--- wiseStepApp/src/main/java/com/wiseStep/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wiseStep.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wiseStep.userInput.CurrentSession;
-
-public interface LoginRepository extends JpaRepository<CurrentSession, String> {
-
-}
